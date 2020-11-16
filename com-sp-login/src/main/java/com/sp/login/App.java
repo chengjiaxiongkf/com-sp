a8b92f38cf661740a701d2a2d9a0467d5b0e0b6a
v_jxjxcheng <aa0070AAU>
2020-11-16 11:17:23 +0800

    优化注解

--- com-sp-login/src/main/java/com/sp/login/App.java
+++ com-sp-login/src/main/java/com/sp/login/App.java
@@ -1,17 +1,13 @@
 package com.sp.login;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author v_jxjxcheng
  * @date 2020-11-12 10:44
  */
 @SpringBootApplication(scanBasePackages = {"com.sp.login"})
-@EnableDiscoveryClient //这是一个nacos客户端
-@Slf4j
 public class App {
     public static void main(String[] args) {
         new SpringApplication(App.class).run(args);
