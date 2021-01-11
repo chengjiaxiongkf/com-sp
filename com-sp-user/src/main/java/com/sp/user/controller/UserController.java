a11ddfac996f77d7bd1ef5bde1064866ecc23e74
v_jxjxcheng <aa0070AAU>
2021-01-11 20:35:37 +0800

    springboot integrate kafka

--- com-sp-user/src/main/java/com/sp/user/controller/UserController.java
+++ com-sp-user/src/main/java/com/sp/user/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.sp.user.controller;
 
+import com.sp.common.dto.ResultDTO;
 import com.sp.user.api.LoginApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value="user")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -20,5 +25,14 @@ public class UserController {
     @GetMapping("/testMethod")
     public String testMethod(){
         return "我从用户调用登录服务->"+loginApi.testMethod();
+    }
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+    // 发送消息
+    @GetMapping("/kafka/sendMsg/{message}")
+    public ResultDTO sendMessage1(@PathVariable("message") String normalMessage) {
+        log.info("send msg for kafka:"+normalMessage);
+        kafkaTemplate.send("noKeyMsg", normalMessage);
+        return new ResultDTO("成功");
     }
 }
